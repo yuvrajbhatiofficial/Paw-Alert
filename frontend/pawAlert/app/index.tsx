18707f1143bab6b6d1a56018ca58375b492fe3a0
mohammed kaif <kaifofficial67@gmail.com>
2024-12-01 20:16:57 +0530

    update frontend

--- frontend/pawAlert/app/index.tsx
+++ frontend/pawAlert/app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View, TextInput, Button, Alert } from "react-native";
 
 export default function Index() {
+  const [name, setName] = useState("")
+
+
+  const handleSubmit =async() =>{
+    const animalData = {name};
+
+    const respose = await fetch("",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(animalData),
+
+    })
+    alert("data submited")
+    
+
+  }
+  
+ 
+ 
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>mohammed kaif</Text>
+      <View className="submitform">
+
+        <TextInput 
+        value={name}
+        onChangeText={(text => setName(text))}
+         placeholder="Enter your name" />
+
+        <Button title="Submit" onPress={handleSubmit} />
+      </View>
     </View>
   );
 }
